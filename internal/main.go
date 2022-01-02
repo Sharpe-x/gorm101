e0f594a3b9af584c4c1dfdc0c66e29e154efc5a4
Sharpe-x <1051383327@qq.com>
2022-01-02 18:04:19 +0800

    learn gorm crud-c

--- internal/main.go
+++ internal/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
@@ -41,6 +42,24 @@ type User struct {
 	UpdateOn int64 `gorm:"autoUpdateTime"`
 }
 
+func initTable(m gorm.Migrator) error {
+	if !m.HasTable(&User{}) {
+		err := m.CreateTable(&User{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// BeforeCreate https://gorm.io/zh_CN/docs/hooks.html hook 函数
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.Age == 0 {
+		u.Age = 20
+	}
+	return
+}
+
 func main() {
 	dsn := viper.GetString("DbConfig.DSN")
 	// 方式一 简单
@@ -72,17 +91,8 @@ func main() {
 	}
 
 	// Test CRUD
-	testCreate(db)
-}
-
-func initTable(m gorm.Migrator) error {
-	if !m.HasTable(&User{}) {
-		err := m.CreateTable(&User{})
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	//testCreate(db)
+	testQuery(db)
 }
 
 func testCreate(gormDb *gorm.DB) {
@@ -223,10 +233,228 @@ func testCreate(gormDb *gorm.DB) {
 	// TODO
 }
 
-// BeforeCreate https://gorm.io/zh_CN/docs/hooks.html hook 函数
-func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
-	if u.Age == 0 {
-		u.Age = 20
+func testQuery(gormDb *gorm.DB) {
+
+	firstUser := &User{}
+	// 获取第一条记录（主键升序）
+	result := gormDb.First(&firstUser)
+	if result.Error != nil {
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("First RecordNotFound")
+			return
+		}
+
+		fmt.Println(result.Error.Error())
+		return
 	}
-	return
+	fmt.Printf("firstUser = %+v\n", firstUser)
+
+	takeUser := new(User)
+	// 获取一条记录，没有指定排序字段
+	result = gormDb.Take(&takeUser)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Take RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("takeUser = %+v\n", takeUser)
+
+	lastUser := new(User)
+	// 获取最后一条记录（主键降序)
+	result = gormDb.Last(&lastUser)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Last RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("lastUser = %+v\n", lastUser)
+	// First 和 Last 会根据主键排序，分别查询第一条和最后一条记录。 只有在目标 struct 是指针或者通过 db.Model() 指定 model 时，该方法才有效。
+	//	此外，如果相关 model 没有定义主键，那么将按 model 的第一个字段进行排序
+	// 如果你想避免ErrRecordNotFound错误，你可以使用Find，比如db.Limit(1).Find(&user)，Find方法可以接受struct和slice的数据。
+
+	userMap := map[string]interface{}{}
+	// 通过 db.Model() 指定 model
+	result = gormDb.Model(&User{}).Last(&userMap)
+	// gormDb.Table("users").First(&result) 这种用法在 First 和 Last是 不可以的!!!!!! 但是take 可以
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Model Last RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("userMap = %+v\n", userMap)
+
+	// 用主键检索
+	primaryKeyUser := new(User)
+	result = gormDb.First(&primaryKeyUser, 10)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("primaryKey = 10 is RecordNotFound")
+		} else {
+			fmt.Println(result.Error.Error())
+			return
+		}
+	}
+
+	result = gormDb.First(&primaryKeyUser, 204)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Model Last RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("primaryKeyUser = %+v\n", primaryKeyUser)
+
+	var users []User
+	result = gormDb.Find(&users, []int{100, 101, 102})
+	if result.Error != nil {
+		// Find 会避免ErrRecordNotFound错误
+		/*		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				fmt.Println("Find users RecordNotFound")
+				return
+			}*/
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("users = %+v\n", users)
+
+	result = gormDb.Find(&users, []int{200, 201, 202})
+	// SELECT * FROM users WHERE id IN (200,201,202);
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("users[%d] = %+v\n", len(users), users)
+
+	firstUserById := new(User)
+	result = gormDb.First(&firstUserById, "id = ?", 206)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Find users RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("firstUserById = %+v\n", firstUserById)
+
+	var allUser []User
+	//获取全部记录
+	result = gormDb.Find(&allUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("allUser[%d] = %+v\n", len(allUser), allUser)
+
+	// String 条件
+	var whereFirstUser User
+	// 获取第一条匹配的记录
+	result = gormDb.Where("name = ?", "sharpe-batches-3").First(&whereFirstUser)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("Find users RecordNotFound")
+			return
+		}
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereFirstUser= %+v\n", whereFirstUser)
+
+	var whereAllUser []*User
+	// 获取全部匹配的记录
+	result = gormDb.Where("name <> ?", "sharpe-batches-3").Find(&whereAllUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereAllUser len =  %d\n", len(whereAllUser))
+
+	// 获取全部匹配的记录
+	result = gormDb.Where("name in ?", []string{"sharpe-batches-1", "sharpe-skip-hook"}).Find(&whereAllUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereAllUser len =  %d\n", len(whereAllUser))
+
+	/*// LIKE
+	db.Where("name LIKE ?", "%jin%").Find(&users)
+	// SELECT * FROM users WHERE name LIKE '%jin%';
+
+	// AND
+	db.Where("name = ? AND age >= ?", "jinzhu", "22").Find(&users)
+	// SELECT * FROM users WHERE name = 'jinzhu' AND age >= 22;
+
+	// Time
+	db.Where("updated_at > ?", lastWeek).Find(&users)
+	// SELECT * FROM users WHERE updated_at > '2000-01-01 00:00:00';
+
+	// BETWEEN
+	db.Where("created_at BETWEEN ? AND ?", lastWeek, today).Find(&users)*/
+
+	// Struct & Map 条件
+	result = gormDb.Where(&User{Age: 20}).Find(&whereAllUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereAllUser len =  %d\n", len(whereAllUser))
+
+	// 实际上查到了全部 主要是当使用结构作为条件查询时，GORM 只会查询非零值字段。这意味着如果您的字段值为 0、''、false 或其他 零值
+	// 如果想要包含零值查询条件，你可以使用 map，其会包含所有 key-value 的查询条件
+	result = gormDb.Where(&User{Age: 0}).Find(&whereAllUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereAllUser len =  %d\n", len(whereAllUser))
+
+	filters := map[string]interface{}{
+		"Age":  0,
+		"Name": "sharpe-skip-hook",
+	}
+
+	// 得到预期
+	result = gormDb.Where(filters).Find(&whereAllUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereAllUser len =  %d\n", len(whereAllUser))
+
+	// 指定结构体查询字段
+	// 等价于 SELECT * FROM t_users WHERE age = 0;
+	var ageUser User
+	result = gormDb.Where(&User{
+		Name: "lala",
+	}, "Age").First(&ageUser)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("whereFirstUser =  %v\n", ageUser)
+	// 查询条件也可以被内联到 First 和 Find 之类的方法中，其用法类似于 Where。
+
+	var findAllUser []User
+	result = gormDb.Find(&findAllUser, filters)
+	if result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return
+	}
+	fmt.Printf("findAllUser len =  %d , findAllUser = %v\n", len(findAllUser), findAllUser)
+
+	// Not 条件 用法与 Where 类似
+
 }
